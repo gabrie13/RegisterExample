cf6e8936d6a4814f81a1acf15bce184287f28c18
gabrie13 <gbarrett2@gmail.com>
2017-07-19 21:24:21 -0600

    Add files via upload

---
+++ Models/RegisterDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculationTest.Models
+{
+    public class RegisterDto
+    {
+        [Key]
+        public int     RegisterId       { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Cash             { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Visa             { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Mastercard       { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Amex             { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Discover         { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Total            { get; set; }
+
+        
+
+    }
+}
---
+++ Services/CalculationService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using CalculationTest.Models;
+
+namespace CalculationTest.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public List<RegisterDto> GetAll()
+        {
+           var registerList = db.Registers.ToList();
+//           List<RegisterViewModel> viewList = new List<RegisterViewModel>();
+//            for(var reg in registerList){
+//                viewList.Add(new RegisterViewModel{
+//                    RegisterId = reg.Id,
+//                    Cash = reg.Cash,
+//                    Visa = reg.Visa,
+//                    Amex = reg.Amex,
+//                    Mastercard = reg.Mastercard,
+//                    Discover = reg.Discover
+//                });
+//           }
+           return registerList.Select(reg => ToDto(reg)).ToList();
+        }
+
+        private static RegisterDto ToDto(Register reg)
+        {
+            return new RegisterDto
+            {
+                RegisterId = reg.RegisterId,
+                Cash = reg.Cash,
+                Visa = reg.Visa,
+                Amex = reg.Amex,
+                Mastercard = reg.Mastercard,
+                Discover = reg.Discover,
+                Total = reg.Cash + reg.Visa + reg.Amex + reg.Mastercard + reg.Discover
+
+            };
+        }
+
+        public RegisterDto FindById(int id)
+        {
+            var register = db.Registers.Find(id);
+            return register != null ? ToDto(register) : null;
+            
+        }
+
+        
+
+        public RegisterDto Create(RegisterDto register)
+        {
+            var reg = new Register
+            {
+                Cash = register.Cash,
+                Visa = register.Visa,
+                Amex = register.Amex,
+                Mastercard = register.Mastercard,
+                Discover = register.Discover
+            };
+            db.Registers.Add(reg);
+            db.SaveChanges();
+
+            register.RegisterId = reg.RegisterId;
+            return register;
+        }
+
+        public RegisterDto Save(RegisterDto register)
+        {
+            var reg = new Register
+            {
+                RegisterId = register.RegisterId,
+                Cash = register.Cash,
+                Visa = register.Visa,
+                Mastercard = register.Mastercard,
+                Amex = register.Amex,
+                Discover = register.Discover
+            };
+
+            db.Entry(reg).State = EntityState.Modified;
+            db.SaveChanges();
+
+            register.RegisterId = reg.RegisterId;
+            return register;
+        }
+
+        
+
+
+    }
+}
